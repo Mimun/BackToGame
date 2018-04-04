8c28e22200b7b15901abb9f33f3a23554c424365
Phan Hoang Anh <hoanganh17b@gmail.com>
2018-04-04 17:11:03 +0700

    add file

---
+++ Devs/Labs/C#/GameFoundation/GameFoundation/StartPoint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Fleck;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GameFoundation
+{
+    class StartPoint
+    {
+        public static List<IWebSocketConnection> socketList = new List<IWebSocketConnection>();
+        public static IWebSocketServer gameServer = new WebSocketServer("ws://0.0.0.0:8765");
+        static void Main(string[] args)
+        {
+            gameServer.Start(socket =>
+            {
+                
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    socketList.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    socketList.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    socketList.ToList().ForEach(s => s.Send("Echo: " + message));
+                };
+            });
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in socketList.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+
+        }
+
+
+    }
+}
---
+++ Devs/Labs/C#/GameFoundation/GameFoundation/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive.Linq;
+
+namespace GameFoundation
+{
+    public static class Utils
+    {
+        public static IDisposable setTimeout(Action action, double timeSpan)
+        {
+            var result = Observable.Timer(TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(timeSpan)).Subscribe(_=>action?.Invoke());
+            return result;
+        }
+        public static IDisposable setInterval(Action action, int timeSpan) {
+            return null;
+        }
+    }
+}
